d418e4cd9ed7a1621e5cafa8d861c1cd9c9e0527
German <german.s.1993@yandex.ru>
2022-07-18 17:30:57 +0300

    feat: add reviews section

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  centeredSlides: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
